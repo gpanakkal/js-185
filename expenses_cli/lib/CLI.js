a4a278c9f296dff271ddcb293322458d1c616f2f
Gautam Panakkal <gautampanakkal@gmail.com>
2024-05-20 16:00:42 -0700

    mostly working oop version

--- expenses_cli/lib/CLI.js
+++ expenses_cli/lib/CLI.js
@@ -1,5 +1,55 @@
-class CLI {
+const { ExpenseData } = require("./ExpenseData");
 
+// const COMMANDS = [
+//   'add AMOUNT MEMO [DATE] - record a new expense',
+//   'clear - delete all expenses',
+//   'list - list all expenses',
+//   'delete NUMBER - remove expenses with id NUMBER',
+//   'search QUERY - list expenses with a matching memo field',
+// ];
+
+class CLI {
+  static COMMANDS = [
+    'add AMOUNT MEMO [DATE] - record a new expense',
+    'clear - delete all expenses',
+    'list - list all expenses',
+    'delete NUMBER - remove expenses with id NUMBER',
+    'search QUERY - list expenses with a matching memo field',
+  ];
+
+  constructor() {
+    this.expenseData = new ExpenseData();
+  }
+  static handleInput() {
+    const operation = process.argv[2];
+    
+    const OPERATION_MAP = {
+      list: ExpenseData.listExpenses,
+      add: CLI.addExpenseInput,
+    }
+  
+    if (!operation) {
+      CLI.showHelp();  
+    } else if (operation in OPERATION_MAP) {
+      OPERATION_MAP[operation]();
+    } else {
+      console.log('Argument unrecognized');
+    }
+  }
+
+  static showHelp() {
+    const help = 'An expense recording system \n\nCommands:\n\n' + CLI.COMMANDS.join('\n');
+    console.log(help);
+  }
+
+  static addExpenseInput() {
+    const [amount, memo, date] = process.argv.slice(3, 6);
+    if (!amount || !memo) {
+      console.log('You must provide an amount and memo.');
+    } else {
+      ExpenseData.addExpense({ amount, memo, date });
+    }
+  }
 }
 
 module.exports = { CLI };
--- expenses_cli/lib/ExpenseData.js
+++ expenses_cli/lib/ExpenseData.js
@@ -1,4 +1,71 @@
+const { Client } = require('pg');
+
+const AMOUNT_MIN_WIDTH = 12;
+const COLUMN_SEPARATOR = ' | ';
+
 class ExpenseData {
+  static logAndExit(rejectedPromise) {
+    console.error(rejectedPromise);
+    process.exit(1);
+  }
+
+  static normalizeCellWidths(data) {
+    // for each column, get the width of the widest cell
+    Object.keys(data.rows[0]).map((key) => {
+      // reduce over column, returning the max length
+      const maxWidth = data.rows
+      .reduce((max, current) => max < current[key].length ? current[key].length : max, '');
+      // pad the start of all column cells to match the widest
+      data.rows.forEach((row) => {
+        row[key] = String(row[key]).padStart(maxWidth, ' ');
+      });
+    });
+  }
+
+  static getFormattedRows(data) {
+    // transform rows into formatted strings
+    ExpenseData.normalizeCellWidths(data);
+    // order fields and concatenate row values
+    const displayRows = data.rows.map((row) => {
+      const formattedDate = new Date(row.created_on).toDateString();
+      const formattedAmount = row.amount.padStart(AMOUNT_MIN_WIDTH, ' ');
+      const orderedRow = [row.id, formattedDate, formattedAmount, row.memo];    
+      return orderedRow.join(COLUMN_SEPARATOR);
+    });
+  
+    return displayRows;
+  }
+
+  constructor() {
+    this.client = new Client();
+  }
+
+  static async _logQuery(queryString) {
+    let data;
+    await this.client.connect().catch(ExpenseData.logAndExit);
+    data = await this.client.query(queryString).catch(ExpenseData.logAndExit);
+    await this.client.end().catch(ExpenseData.logAndExit);
+  
+    const displayRows = ExpenseData.getFormattedRows(data);
+    displayRows.forEach((displayRow) => console.log(displayRow));
+    await this.client.end();
+  }
+
+  static listExpenses() {
+    ExpenseData._logQuery('SELECT * FROM expenses ORDER BY created_on ASC');
+  }
+
+  static async addExpense({ amount, memo, date = new Date() }) {
+    const formattedDate = date.toLocaleDateString();
+    await this.client.connect().catch(ExpenseData.logAndExit);
+    const success = await this.client.query(
+      'INSERT INTO expenses (amount, memo, created_on) VALUES ($1, $2, $3)',
+      [amount, memo, formattedDate]
+    ).catch(ExpenseData.logAndExit);
+    // console.log({ success: Array.isArray(success.rows) });
+    await this.client.end().catch(ExpenseData.logAndExit);
+  }
+
 
 }
 
